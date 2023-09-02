ec8d9857cb847950e448099aa78ca21721adb5a3
Imperfiction <10811551+imfi-jz@users.noreply.github.com>
2023-09-02 19:48:30 +0200

    Refactored GameRuleEvents so they are concerned with handling only one game

--- src/nl/imfi_jz/battlesofdestinyre/event/game/GameRuleEvent.hx
+++ src/nl/imfi_jz/battlesofdestinyre/event/game/GameRuleEvent.hx
@@ -4,14 +4,31 @@ import nl.imfi_jz.battlesofdestinyre.game.InitializedGame;
 import nl.imfi_jz.functional.collection.Collection.Multitude;
 import nl.imfi_jz.minecraft_api.Event;
 
-abstract class GameRuleEvent implements Event {
+abstract class GameRuleEvent implements CancelingEvent {
     private final games:Multitude<InitializedGame>;
 
     public function new(initializedGames:Multitude<InitializedGame>) {
         this.games = initializedGames;
     }
 
-    public function getInitializedGames():Multitude<InitializedGame> {
+    public final function getInitializedGames():Multitude<InitializedGame> {
         return games;
     }
+
+    private abstract function occurForGame(involvement:EventData, game:InitializedGame):Void;
+    
+    private function shouldCancelForGame(involvement:EventData, game:InitializedGame):Bool {
+        return false;
+    }
+
+    public final function occur(involvement:EventData) {
+        getInitializedGames().each((game) -> occurForGame(involvement, game));
+    }
+
+	public final function shouldCancel(involvement:EventData):Bool {
+        return getInitializedGames().reduce(
+            false,
+            (shouldCancel, game) -> shouldCancel || shouldCancelForGame(involvement, game)
+        );
+	}
 }
--- src/nl/imfi_jz/battlesofdestinyre/event/game/PlayerDamagePlayerEvent.hx
+++ src/nl/imfi_jz/battlesofdestinyre/event/game/PlayerDamagePlayerEvent.hx
@@ -1,5 +1,6 @@
 package nl.imfi_jz.battlesofdestinyre.event.game;
 
+import nl.imfi_jz.battlesofdestinyre.game.InitializedGame;
 import nl.imfi_jz.battlesofdestinyre.state.StateKey;
 import nl.imfi_jz.minecraft_api.GameObject.Encounter;
 import nl.imfi_jz.functional.collection.Collection.Multitude;
@@ -15,29 +16,24 @@ class PlayerDamagePlayerEvent extends GameRuleEvent implements CancelingEvent {
 		return "EntityDamageByEntityEvent";
 	}
 
-	public function occur(involvement:EventData) {
-
+	private function occurForGame(involvement:EventData, game:InitializedGame) {
+        
     }
 
-	public function shouldCancel(involvement:EventData):Bool {
+    private override function shouldCancelForGame(involvement:EventData, game:InitializedGame):Bool {
         final players:Multitude<Encounter> = involvement.getEncounters().filter((encounter) -> encounter.isA("player"));
 
         if(players.length > 1){
-            return games.reduce(false, (cancel, game) -> {
-                if(cancel) {
-                    return true;
-                }
-
-                final playersInGame = game.getOnlinePlayers().reduce(0, (count, player) -> {
-                    return count + players.filter((otherPlayer) -> player.matches(otherPlayer)).length;
-                });
-
-                return playersInGame < 2
-                    || game.getMemoryGameState().getBool(StateKey.stageAllowPvp(
-                            game.getMemoryGameState().getString(StateKey.STAGE)
-                        )) != true;
+            final playersInGame = game.getOnlinePlayers().reduce(0, (count, player) -> {
+                return count + players.filter((otherPlayer) -> player.matches(otherPlayer)).length;
             });
+
+            // Cancel if the event is not about two players in the same game or if the game does not allow pvp
+            return playersInGame < 2
+                || game.getMemoryGameState().getBool(StateKey.stageAllowPvp(
+                        game.getMemoryGameState().getString(StateKey.STAGE)
+                    )) != true;
         }
         else return false;
-	}
+    }
 }
